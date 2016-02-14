87363d3bd7e4b644d5273a7de9bf08717bb8ca6a
Artiym <artiym9696@gmail.ru>
2016-02-14 15:41:03 +0300

    hjkhj

---
+++ Liga/Liga/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace Liga
+{
+   
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var cl = new WebClient();
+            var res = cl.DownloadString(new Uri("http://api.lod-misis.ru/testassignment"));
+            var tbl = JArray.Parse(res)
+                .Select(j => new
+                {
+                    PlayerName = j["PlayerName"],
+                    Team = j["Team"],
+                    Score = j["Score"]
+                });
+            foreach (var v in tbl.OrderByDescending(i => i.Score))
+                
+                Console.WriteLine(v.PlayerName + "\t" + v.Team + "\t" + v.Score); 
+        Console.ReadKey();
+
+        }
+    }
+}
